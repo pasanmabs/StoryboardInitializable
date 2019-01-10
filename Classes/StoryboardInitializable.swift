4865c92a905c891375069fe54546ebc963ed075e
Pasan <pasan@mabotuwana.com>
2019-01-10 12:02:06 +1100

    Initial Commit

---
+++ Classes/StoryboardInitializable.swift
@@ -0,0 +1,40 @@
+//
+//  StoryboardInitializable.swift
+//  StoryboardInitializable
+//
+//  Created by Pasan Mabotuwana on 10/1/19.
+//  Copyright © 2019 Pasan Mabotuwana. All rights reserved.
+//
+
+import UIKit
+
+/// Create a StoryboardInitializable protocol
+protocol StoryboardInitializable {
+    
+    /// Name of the storyboard file to load
+    static var storyboardName: String { get }
+    
+}
+
+/// Set defaults
+extension StoryboardInitializable where Self: UIViewController {
+    
+    // MARK: - Default implementation
+    
+    /// Uses the current class name
+    static var storyboardName: String {
+        return String(describing: self)
+    }
+    
+    /// Get the current storyboard
+    static func initFromStoryboard() -> Self {
+        let storyboard = UIStoryboard(name: storyboardName, bundle: Bundle.main)
+        
+        if let viewController = storyboard.instantiateInitialViewController() as? Self {
+            return viewController
+        } else {
+            fatalError("Cannot init storyboard with name \(storyboardName)")
+        }
+    }
+    
+}
---
+++ StoryboardInitializable.podspec
@@ -0,0 +1,20 @@
+Pod::Spec.new do |s|
+  s.name             = 'StoryboardInitializable'
+  s.version          = '0.1.0'
+  s.summary          = 'By far the easiest way to init a storyboard.'
+ 
+  s.description      = <<-DESC
+Easily initalise a view controller from a storyboard.
+                       DESC
+ 
+  s.homepage         = 'https://github.com/pasanmabs/StoryboardInitializable'
+  s.license          = { :type => 'MIT', :file => 'LICENSE' }
+  s.author           = { 'Pasan Mabotuwana' => 'pasan@mabotuwana.com' }
+  s.source           = { :git => 'https://github.com/pasanmabs/StoryboardInitializable.git', :tag => s.version.to_s }
+ 
+ s.ios.deployment_target = '11.0'
+  s.swift_version = '4.2'
+  
+  s.source_files = 'Classes/StoryboardInitializable.swift'
+ 
+end
